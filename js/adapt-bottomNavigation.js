6465c335397180de9fd12a01f47942d435628a66
Oliver Foster <oliver.foster@kineo.com>
2019-01-25 12:20:37 +0000

    Changed framework dependency and added any helper polyfill

--- js/adapt-bottomNavigation.js
+++ js/adapt-bottomNavigation.js
@@ -2,8 +2,26 @@ define([
     'core/js/adapt',
     './model',
     './view',
-    './lib/adaptModelExtension'
-], function(Adapt, Model, View) {
+    'handlebars'
+], function(Adapt, Model, View, Handlebars) {
+
+    if (!Handlebars.helpers.any) {
+        // Polyfill for older frameworks.
+        /**
+         * Equivalent to:
+         *  if (conditionA || conditionB)
+         * @example
+         * {{#any displayTitle body instruction}}
+         * <div class="component__header {{_component}}__header"></div>
+         * {{/any}}
+         */
+        Handlebars.registerHelper('any', function() {
+            var args = Array.prototype.slice.call(arguments, 0, -1);
+            var block = Array.prototype.slice.call(arguments, -1)[0];
+
+            return _.any(args) ? block.fn(this) : block.inverse(this);
+        });
+    }
 
     return Adapt.register("bottomnavigation", {
         model: Model,
